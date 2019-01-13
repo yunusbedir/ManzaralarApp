9d5237afda7f438425b3ac8e62613fb923a201c9
yunus bedir <viper.yns@outlook.com>
2019-01-13 11:40:11 +0300

    Adapter fonksiyonlarına işler verildi.RecyclerView e adapter atandı.

--- app/src/main/java/com/example/yunus/manzaralar/MainActivity.kt
+++ app/src/main/java/com/example/yunus/manzaralar/MainActivity.kt
@@ -2,11 +2,38 @@ package com.example.yunus.manzaralar
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var tumManzaralar = ArrayList<Manzara>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        manzaraDoldur()
+
+        var myAdapter = ManzaraAdapter(tumManzaralar)
+
+        rvManzaralar.adapter = myAdapter
+        //RecyclerView ın görüntüleme tarzını belirliyoruz.burada linearLayout Vertical olsun diyoruz istersek
+        //GridView vb. yapılabilir
+        var linearLayoutManager = LinearLayoutManager(this,LinearLayout.VERTICAL,false)
+        rvManzaralar.layoutManager = linearLayoutManager
+    }
+
+    fun manzaraDoldur() : ArrayList<Manzara>{
+        var manzaralar = arrayOf(R.drawable.t_1_0 , R.drawable.t_1_1 ,R.drawable.t_1_2 ,R.drawable.t_1_3 ,R.drawable.t_1_4,R.drawable.t_1_5,R.drawable.t_1_6
+                ,R.drawable.t_2_0,R.drawable.t_2_1,R.drawable.t_2_3
+            ,R.drawable.t_3_0,R.drawable.t_3_1,R.drawable.t_3_2,R.drawable.t_3_4,R.drawable.t_3_6
+            ,R.drawable.t_4_0,R.drawable.t_4_1)
+
+        for (i in 0..(manzaralar.size - 1)){
+            var atanacakManzara = Manzara("MANZARA " + i , "Açıklama " + i , manzaralar[i])
+            tumManzaralar.add(atanacakManzara)
+        }
+        return tumManzaralar
     }
 }
--- app/src/main/java/com/example/yunus/manzaralar/ManzaraAdapter.kt
+++ app/src/main/java/com/example/yunus/manzaralar/ManzaraAdapter.kt
@@ -1,26 +1,40 @@
 package com.example.yunus.manzaralar
 
+import android.support.v7.widget.CardView
 import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.tek_satir.view.*
 
-class ManzaraAdapter : RecyclerView.Adapter<ManzaraAdapter.ManzaraViewHolder>() {
+class ManzaraAdapter(tumManzaralar : ArrayList<Manzara>) : RecyclerView.Adapter<ManzaraAdapter.ManzaraViewHolder>() {
 
+    var manzaralar = tumManzaralar
     override fun onCreateViewHolder(parent: ViewGroup, position: Int): ManzaraViewHolder {
         //tek_satir.xml dosyasını java sınıfına dönüştüreceğiz.Ardından ViewHolder oluşturup geri döndüreceğiz
+        var inflater = LayoutInflater.from(parent.context)
+        var tekSatirManzara = inflater.inflate(R.layout.tek_satir,parent,false)
+
+        return ManzaraViewHolder(tekSatirManzara)
 
     }
 
     override fun getItemCount(): Int {
         //item sayısı
-        return 0
+        return manzaralar.size
     }
 
     override fun onBindViewHolder(holder: ManzaraViewHolder, position: Int) {
         //holderı listeye atayacağız
+        holder.manzaraBaslik.text = manzaralar[position].baslik
+        holder.manzaraAciklama.text = manzaralar[position].aciklama
+        holder.manzaraResim.setImageResource(manzaralar[position].resim)
     }
 
     class ManzaraViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
-
+        var tekSatirManzara = itemView as CardView
+        var manzaraBaslik =  tekSatirManzara.tvManzaraBaslik
+        var manzaraAciklama = tekSatirManzara.tvManzaraAciklama
+        var manzaraResim = tekSatirManzara.imgManzara
     }
 }
